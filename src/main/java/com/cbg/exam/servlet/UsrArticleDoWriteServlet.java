cb9a33fba154bc224b8f65768c277caa1a955f53
bgchoi205 <choibg205@naver.com>
2021-06-21 13:29:24 +0900

    게시물 작성처리

--- src/main/java/com/cbg/exam/servlet/UsrArticleDoWriteServlet.java
+++ src/main/java/com/cbg/exam/servlet/UsrArticleDoWriteServlet.java
@@ -2,12 +2,14 @@ package com.cbg.exam.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.cbg.mysqlutil.MysqlUtil;
+import com.cbg.mysqlutil.SecSql;
 
 
 @WebServlet("/usr/article/doWrite")
@@ -26,11 +28,24 @@ public class UsrArticleDoWriteServlet extends HttpServlet {
 		// HTML이 UTF-8 형식이라는 것을 브라우저에 알림.
 		response.setContentType("text/html; charset=UTF-8");
 		
+		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "jsp_board");
+
+		MysqlUtil.setDevMode(true);
+		
 		String title = request.getParameter("title");
 		String body = request.getParameter("body");
 		
-		response.getWriter().append(title + "<br>");
-		response.getWriter().append(body + "<br>");
+		SecSql sql = new SecSql();
+		sql.append("INSERT INTO article");
+		sql.append("SET regDate = NOW()");
+		sql.append(", updateDate = NOW()");
+		sql.append(", title = ?", title);
+		sql.append(", `body` = ?", body);
+		int id = MysqlUtil.insert(sql);
+		
+		response.getWriter().append(id + "번 게시물 생성 완료");
+		
+		MysqlUtil.closeConnection();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
